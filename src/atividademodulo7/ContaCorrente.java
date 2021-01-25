f3eaee6a82055956c6df8420af923785642a854f
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-01-25 20:51:55 -0300

    Criando funções para saque em conta corrente

--- src/atividademodulo7/ContaCorrente.java
+++ src/atividademodulo7/ContaCorrente.java
@@ -5,13 +5,17 @@ package atividademodulo7;
  * @author Victor
  */
 public class ContaCorrente extends Conta{
-    private Double saldoChequeEspecial;
+    private double saldoChequeEspecial;
 
     public ContaCorrente(String nomeBanco, Double saldo, Integer numeroAgencia, Integer numeroConta, Double saldoChequeEspecial) {
         super(nomeBanco, saldo, numeroAgencia, numeroConta);
         this.saldoChequeEspecial = saldoChequeEspecial;
     }
 
+    public void setSaldoChequeEspecial(double saldoChequeEspecial) {
+        this.saldoChequeEspecial = saldoChequeEspecial;
+    }
+    
     @Override
     public String toString() {
         return "ContaCorrente{" + "saldoChequeEspecial=" + saldoChequeEspecial + '}';
@@ -24,20 +28,38 @@ public class ContaCorrente extends Conta{
     
     @Override
     public void saque(double valorSaque){
-        if (valorSaque <= this.getSaldo() - this.saldoChequeEspecial){
-            this.saldo -= valorSaque;
+        if (verificadorPossivelSacarSemChequeEspecial(valorSaque)){
+            saqueSemChequeEspecial(valorSaque);
             System.out.println("Saque sem utilizar cheque especial\n "
                     + "Valor atual: R$ " + this.getSaldo());
         }
-        else if (valorSaque <= this.getSaldo()){
-            valorSaque -= this.saldo;
-            this.saldo = 0;
-            this.saldoChequeEspecial -= valorSaque;
+        else if (verificadorPossivelSacarComChequeEspecial(valorSaque)){
+            saqueComChequeEspecial(valorSaque);
             System.out.println("Saque utilizando cheque especial\n "
                     + "Valor atual: R$ " + this.getSaldo());
         }
         else System.out.println("Valor insuficiente!");
     } 
     
+    private void saqueComChequeEspecial(double valorSaque){
+        valorSaque -= this.saldo;
+        zerarSaldo();
+        this.setSaldoChequeEspecial(this.saldoChequeEspecial - valorSaque);
+    }
     
+    private void saqueSemChequeEspecial(double valorSaque){
+       this.saldo -= valorSaque;
+    }
+    
+    private boolean verificadorPossivelSacarComChequeEspecial (double valorVerificado){
+        return valorVerificado <= this.getSaldo();
+    }
+    
+    private boolean verificadorPossivelSacarSemChequeEspecial (double valorVerificado){
+        return valorVerificado <= this.getSaldo() - this.saldoChequeEspecial;
+    }
+    
+    private void zerarSaldo(){
+        this.saldo = 0;
+    } 
 }
