0583ef0148b2d93d85b9cf08f5e2b17015a51c63
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-01-27 12:53:41 -0300

    Criando objeto ContaSalario

--- src/atividademodulo7/ContaSalario.java
+++ src/atividademodulo7/ContaSalario.java
@@ -9,8 +9,37 @@ package atividademodulo7;
  *
  * @author Victor
  */
-public class ContaSalario {
-    private int limiteSaque;
+public class ContaSalario extends Conta{
+    private final int limiteSaques;
+    private int saquesFeitos;
+
+    public ContaSalario(String nomeBanco, double saldo, Integer numeroAgencia, Integer numeroConta, int limiteSaques) {
+        super(nomeBanco, saldo, numeroAgencia, numeroConta);
+        this.limiteSaques = limiteSaques;
+        this.saquesFeitos = 0;
+    }
+
+    @Override
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    @Override
+    public void saque(double valorSaque) {
+        if (limiteSaques < saquesFeitos){
+            if (valorSaque <= this.getSaldo()){
+                this.setSaldo(this.getSaldo() - valorSaque);
+                this.saquesFeitos++;
+            }else System.out.println("Valor insuficiente!");
+        }else System.out.println("Quantidade de saques excedida");
+    }
+
+    @Override
+    public String saldoConta() {
+        return "Saldo em conta: R$ "+this.saldo;
+    }
     
-    
+    public void limparQuantidadeDeSaques(){
+        this.saquesFeitos = 0;
+    }
 }
